ff390afae687100661d027491a7a6370e920829b
Robert Melendez <robertm3lendez@gmail.com>
2018-11-29 23:57:50 -0500

    fix editing referral and updating

--- controllers/referrals.js
+++ controllers/referrals.js
@@ -23,14 +23,60 @@ exports.getReferral = (req, res, next) => {
   });
 };
 
-
+//add a referral page
 exports.addReferral = (req, res, next) => {
     let title = 'Adding referral';
     let path = 'add-referral';
     res.render('referrals/add-referral', { title: title, path: path });
   };
 
+// editing a referral 
+exports.editReferral = (req, res, next) => {
+    const id = req.params.id;
+    Referral.findOne({_id: id})
+    .then(referral => {
+      res.render('referrals/referral-edit', {referral: referral, title: "Editing", path: 'editing'});
+    })
+    .catch(err => console.log(err));
+};
 
+//update referral
+exports.updateReferral = (req, res, next) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const last_name = req.body.last_name;
+  const address = {
+    address: req.body.address,
+    apt: req.body.apt,
+    city: req.body.city,
+    zipcode: req.body.zipcode
+  };
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const referralBy = req.body.referralBy;
+  const comment = req.body.comment;
+  const status = req.body.status;
+  const moveIn = req.body.moveIn;
+
+  Referral.findByIdAndUpdate(id, {
+    name: name,
+    last_name: last_name,
+    address: address,
+    email: email,
+    phone: phone,
+    comment: comment, 
+    referralBy: referralBy,
+    status: status,
+    moveIn: moveIn
+  }).then(referral => {
+    console.log(referral);
+    res.redirect('/detail/'+referral._id);
+  })
+  .catch(err => console.log(err));
+
+}
+
+//adding the referral handler page
 exports.postReferral = (req, res, next) => {
   const name = req.body.name;
   const last_name = req.body.last_name;
--- routes/referrals.js
+++ routes/referrals.js
@@ -9,12 +9,16 @@ const referralController = require('../controllers/referrals');
 //getting all referrals
 router.get('/referrals', referralController.getReferrals);
 
-//adding a nw referral 
+//adding a new referral 
 router.get('/add-referral', referralController.addReferral);
-
+//adding referral route
 router.post('/add-referral', referralController.postReferral);
-
+// referral detail route
 router.get('/detail/:id', referralController.getReferral);
+//editing referral route
+router.get('/referral/edit/:id', referralController.editReferral);
+
+router.post('/referral/update/:id', referralController.updateReferral);
 
 module.exports = router;
 
